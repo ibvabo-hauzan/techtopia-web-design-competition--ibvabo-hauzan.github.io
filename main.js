c6ddaa6a75d5442702231cefdac26cd4ec347ff7
Hauzan Alkhawarizmi <ibvabohauzan@gmail.com>
2026-09-24 12:53:03 +0700

    update 0.5

--- main.js
+++ main.js
@@ -9,23 +9,25 @@ function removeClassListByClassName(element,classlist){
     }
 }
 function darkLight(){
-    if(document.getElementById("darklight").innerText == "Light Mode") {
+    if(document.getElementById("darklight_symbol").className == "bi bi-sun-fill") {
 
-        document.getElementById("darklight").innerText = "Dark Mode"
+        document.getElementById("darklight_symbol").className = "bi bi-moon-stars-fill"
         addClassListByClassName("body","light-body")
         addClassListByClassName("darklight","light-darklight")
         addClassListByClassName("brand","light-brand")
         addClassListByClassName("h1-2-3","light-h1-2-3-nav_a")
+        addClassListByClassName("navbar","light-navbar")
         addClassListByClassName("nav_button","light-nav_button")
         addClassListByClassName("p-a-label","light-p-a-label")
 
     } else {
 
-        document.getElementById("darklight").innerText = "Light Mode"
+        document.getElementById("darklight_symbol").className = "bi bi-sun-fill"
         removeClassListByClassName("body","light-body")
         removeClassListByClassName("darklight","light-darklight")
         removeClassListByClassName("brand","light-brand")
         removeClassListByClassName("h1-2-3","light-h1-2-3-nav_a")
+        removeClassListByClassName("navbar","light-navbar")
         removeClassListByClassName("nav_button","light-nav_button")
         removeClassListByClassName("p-a-label","light-p-a-label")
         
